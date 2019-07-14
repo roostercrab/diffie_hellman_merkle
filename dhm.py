25a53dc085dcf584174342e9e01549dcd4f3f21d
roostercrab <jesse.shea@qixingdesign.com>
2019-07-14 11:11:55 -0700

    init commit

---
+++ dhm.py
@@ -0,0 +1,26 @@
+def dhm():
+    alice_secret = int(input("Enter Alice's Secret Number: "))
+    # 882377523950
+    bob_secret = int(input("Enter Bob's Secret Number: "))
+    # 982734869235
+
+    g = 3
+    p = 6840273601699
+
+    print(f'generator: {g}')
+    print(f'prime: {p}')
+
+
+    alice_public = (g ^ alice_secret) % p
+    bob_public = (g ^ bob_secret) % p
+
+    print(f"Alice's Public: {alice_public}")
+    print(f"Bob's Public: {bob_public}")
+
+    alice_bob_private = (bob_public ^ alice_secret) % p
+    bob_alice_private = (alice_public ^ bob_secret) % p
+
+    print(f"Alice's Private Calculation: {alice_bob_private}")
+    print(f"Bob's Private Calculation: {bob_alice_private}")
+
+dhm()
